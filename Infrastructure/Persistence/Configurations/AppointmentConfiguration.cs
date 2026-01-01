d54084bf975382b381f12f7ed72d152398323ece
Parsanb <parsanasiri123@gmail.com>
2026-01-01 10:56:24 +0330

    Setup Configurations : AppointmentConfiguration

---
+++ Infrastructure/Persistence/Configurations/AppointmentConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+public class AppointmentConfiguration : IEntityTypeConfiguration<Appointment>
+{
+    public void Configure(EntityTypeBuilder<Appointment> builder)
+    {
+        builder.HasKey(a => a.Id);
+
+        builder.HasOne<Doctor>()
+               .WithMany(d => d.Appointments)
+               .HasForeignKey(a => a.DoctorId)
+               .OnDelete(DeleteBehavior.Restrict);
+    }
+}

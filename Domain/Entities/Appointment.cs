b0fdb8d555890189c7468414adcb4a9192674186
milad <miladhamedani81@gmail.com>
2026-01-01 12:32:20 +0330

    Edit entities : Navigation Properties

--- Domain/Entities/Appointment.cs
+++ Domain/Entities/Appointment.cs
@@ -5,8 +5,12 @@ namespace Domain.Entities;
 
 public class Appointment : BaseEntity
 {
-    public int PatientId { get; set; }
-    public int DoctorId { get; set; }
-    public DateTime AppointmentDate { get; set; }
-    public AppointmentStatus Status { get; set; }
+    public int PatientId { get; set; } 
+    public int DoctorId { get; set; } 
+    public DateTime AppointmentDate { get; set; } 
+    public AppointmentStatus Status { get; set; } 
+
+    public Doctor Doctor { get; set; } = null!;
+
+    public ICollection<MedicalFile> MedicalFiles { get; set; } = new List<MedicalFile>();
 }

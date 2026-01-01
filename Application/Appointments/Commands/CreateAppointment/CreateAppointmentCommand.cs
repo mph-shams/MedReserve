2cfed43e9d8b339e030eb38e4072d90315a08ffb
Hossein <hossein20812081@gmail.com>
2026-01-01 18:00:58 +0330

    Edit CreateAppointmentCommand

--- Application/Appointments/Commands/CreateAppointment/CreateAppointmentCommand.cs
+++ Application/Appointments/Commands/CreateAppointment/CreateAppointmentCommand.cs
@@ -4,8 +4,8 @@ using Domain.Enums;
 namespace Application.Appointments.Commands.CreateAppointment;
 
 public record CreateAppointmentCommand(
-    Guid DoctorId,
-    Guid PatientId,
+    int DoctorId,
+    int PatientId,
     DateTime AppointmentDate,
     string Description
-) : IRequest<Guid>;
+) : IRequest<int>;

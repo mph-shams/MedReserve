359972acb127954f160c0a7c5b6645c211d37812
milad <miladhamedani81@gmail.com>
2026-01-01 12:31:31 +0330

    Edit Entities: Navigation Properties

--- Domain/Entities/Doctor.cs
+++ Domain/Entities/Doctor.cs
@@ -4,10 +4,13 @@ namespace Domain.Entities;
 
 public class Doctor : BaseEntity
 {
-    public int UserId { get; set; }
-    public string Specialty { get; set; } = string.Empty;
+    public int UserId { get; set; } 
+    public string Specialty { get; set; } = string.Empty; 
     public string Bio { get; set; } = string.Empty;
     public decimal ConsultationFee { get; set; }
 
     public User User { get; set; } = null!;
+
+    public ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
+    public ICollection<Schedule> Schedules { get; set; } = new List<Schedule>();
 }

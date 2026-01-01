38270ec3c37ae60e88320b9759ae41cd1bcfa044
Hossein <hossein20812081@gmail.com>
2026-01-01 17:34:20 +0330

    Commands-Implementation : CreateAppointmentCommand

---
+++ Application/Appointments/Commands/CreateAppointment/CreateAppointmentCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Domain.Enums; 
+
+namespace Application.Appointments.Commands.CreateAppointment;
+
+public record CreateAppointmentCommand(
+    Guid DoctorId,
+    Guid PatientId,
+    DateTime AppointmentDate,
+    string Description
+) : IRequest<Guid>;

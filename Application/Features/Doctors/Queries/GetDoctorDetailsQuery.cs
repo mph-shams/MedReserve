f3ec8899c7c3f4724e156bd95b9963fd360ae41a
Hossein <hossein20812081@gmail.com>
2026-01-03 12:54:30 +0330

    Create Queries : GetDoctorDetailsQuery

---
+++ Application/Features/Doctors/Queries/GetDoctorDetailsQuery.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+
+namespace MedReserve.Application.Features.Doctors.Queries;
+
+public record DoctorDetailsDto(int Id, string Specialty, string Bio, decimal Fee);
+public record GetDoctorDetailsQuery(int Id) : IRequest<Result<DoctorDetailsDto>>;
+
+public class GetDoctorDetailsHandler(IUnitOfWork _unitOfWork) : IRequestHandler<GetDoctorDetailsQuery, Result<DoctorDetailsDto>>
+{
+    public async Task<Result<DoctorDetailsDto>> Handle(GetDoctorDetailsQuery request, CancellationToken ct)
+    {
+        var doctor = await _unitOfWork.Repository<Domain.Entities.Doctor>().GetByIdAsync(request.Id);
+        if (doctor == null) return Result<DoctorDetailsDto>.Failure("Doctor not Found!");
+        return Result<DoctorDetailsDto>.Success(new DoctorDetailsDto(doctor.Id, doctor.Specialty, doctor.Bio, doctor.ConsultationFee));
+    }
+}

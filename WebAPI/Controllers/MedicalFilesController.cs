7ffaee68c9a1203196085e9bce329e57f89b373b
Hossein <hossein20812081@gmail.com>
2026-01-03 09:44:02 +0330

    Create Controllers : MedicalFilesController

---
+++ WebAPI/Controllers/MedicalFilesController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common.Models;
+using Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedReserve.WebAPI.Controllers
+{
+    [Authorize]
+    [HttpGet("download/{id}")]
+    public async Task<IActionResult> DownloadFile(int id)
+    {
+        var file = await _unitOfWork.Repository<MedicalFile>().GetByIdAsync(id);
+        if (file == null) return NotFound(Result<string>.Failure("File not Found! "));
+
+        var stream = new MemoryStream(file.FileContent);
+        return new FileStreamResult(stream, file.ContentType)
+        {
+            FileDownloadName = file.FileName
+        };
+    }
+}

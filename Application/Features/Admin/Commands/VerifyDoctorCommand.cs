c72d4201d51a201f2da6e4526d9583b72b2601c8
Parsanb <parsanasiri123@gmail.com>
2026-01-03 15:35:09 +0330

    Create Commands : VerifyDoctorCommand

---
+++ Application/Features/Admin/Commands/VerifyDoctorCommand.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+
+namespace MedReserve.Application.Features.Admin.Commands
+{
+    public record VerifyDoctorCommand(int DoctorId) : IRequest<Result<bool>>;
+
+    public class VerifyDoctorHandler(IUnitOfWork _unitOfWork) : IRequestHandler<VerifyDoctorCommand, Result<bool>>
+    {
+        public async Task<Result<bool>> Handle(VerifyDoctorCommand request, CancellationToken ct)
+        {
+            var doctor = await _unitOfWork.Repository<Domain.Entities.Doctor>().GetByIdAsync(request.DoctorId);
+
+            if (doctor == null)
+                return Result<bool>.Failure("The requested doctor was not found.");
+
+            return Result<bool>.Success(true);
+        }
+    }
+}

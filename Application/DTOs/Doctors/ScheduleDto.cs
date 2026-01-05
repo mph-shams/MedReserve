3f31c3dfd77328af64a425abc237a0b0c4f357d3
hossein2081 <117431730+hossein2081@users.noreply.github.com>
2026-01-05 22:47:14 +0330

    Create Dtos : ScheduleDto

---
+++ Application/DTOs/Doctors/ScheduleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MedReserve.Application.DTOs.Doctors
+{
+    public record ScheduleDto(
+        int Id,
+        DayOfWeek DayOfWeek,
+        TimeSpan StartTime,
+        TimeSpan EndTime,
+        int SlotDuration
+    );
+}

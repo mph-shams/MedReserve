f02a40ba50c29b51cfee666d1a6ba575fcec6263
hossein2081 <117431730+hossein2081@users.noreply.github.com>
2026-01-05 22:44:33 +0330

    Create Dtos : UpdateDoctorProfileRequest

---
+++ Application/DTOs/Doctors/UpdateDoctorProfileRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MedReserve.Application.DTOs.Doctors
+{
+    public record UpdateDoctorProfileRequest(
+        string Specialty,
+        string Bio,
+        decimal ConsultationFee
+    );
+}

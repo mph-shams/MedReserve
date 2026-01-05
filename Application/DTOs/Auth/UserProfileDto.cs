1b2b17492dfaaee247fb1a0c087f1ee0a3decce2
hossein2081 <117431730+hossein2081@users.noreply.github.com>
2026-01-05 22:33:53 +0330

    Create Dtos : UserProfileDto

---
+++ Application/DTOs/Auth/UserProfileDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MedReserve.Application.DTOs.Auth
+{
+    public record UserProfileDto(
+        int Id,
+        string Username,
+        string Role,
+        bool IsVerified,
+        DateTime CreatedAt
+    );
+}

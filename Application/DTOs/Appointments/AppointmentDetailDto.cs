4a518f4b57eab34b3b40a8b93a20cc98b80749b1
hossein2081 <117431730+hossein2081@users.noreply.github.com>
2026-01-05 22:32:39 +0330

    Create Dtos : AppointmentDetailDto

---
+++ Application/DTOs/Appointments/AppointmentDetailDto.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.MedicalFiles.Queries.GetMedicalFilesByAppointment;
+
+namespace MedReserve.Application.DTOs.Appointments
+{
+    public record AppointmentDetailDto(
+        int Id,
+        int DoctorId,
+        string DoctorName,
+        DateTime AppointmentDate,
+        string Status,
+        string Description,
+        List<MedicalFileDto> AttachedFiles
+    );
+}

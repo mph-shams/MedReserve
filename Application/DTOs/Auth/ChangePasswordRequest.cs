ef91d13432fc03824a961a1483e9b17f17366906
hossein2081 <117431730+hossein2081@users.noreply.github.com>
2026-01-05 22:39:14 +0330

    Create Dtos : ChangePasswordRequest

---
+++ Application/DTOs/Auth/ChangePasswordRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MedReserve.Application.DTOs.Auth
+{
+    public record ChangePasswordRequest(
+        string CurrentPassword,
+        string NewPassword,
+        string ConfirmNewPassword
+    );
+}

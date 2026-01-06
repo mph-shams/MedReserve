b83b3cac3565024f95c59c949ee772551e53a960
Parsanb <parsanasiri123@gmail.com>
2026-01-06 17:05:29 +0330

    Update Queries : GetDoctorDetailsQuery&GetAllDoctorsQuery

--- Application/Features/Doctors/Queries/GetDoctorDetailsQuery.cs
+++ Application/Features/Doctors/Queries/GetDoctorDetailsQuery.cs
@@ -1,10 +1,10 @@
 ﻿using Application.Common.Interfaces;
 using Application.Common.Models;
+using MedReserve.Application.DTOs.Doctors;
 using MediatR;
 
 namespace MedReserve.Application.Features.Doctors.Queries;
 
-public record DoctorDetailsDto(int Id, string Specialty, string Bio, decimal Fee);
 public record GetDoctorDetailsQuery(int Id) : IRequest<Result<DoctorDetailsDto>>;
 
 public class GetDoctorDetailsHandler(IUnitOfWork _unitOfWork) : IRequestHandler<GetDoctorDetailsQuery, Result<DoctorDetailsDto>>
@@ -12,7 +12,16 @@ public class GetDoctorDetailsHandler(IUnitOfWork _unitOfWork) : IRequestHandler<
     public async Task<Result<DoctorDetailsDto>> Handle(GetDoctorDetailsQuery request, CancellationToken ct)
     {
         var doctor = await _unitOfWork.Repository<Domain.Entities.Doctor>().GetByIdAsync(request.Id);
-        if (doctor == null) return Result<DoctorDetailsDto>.Failure("Doctor not Found!");
-        return Result<DoctorDetailsDto>.Success(new DoctorDetailsDto(doctor.Id, doctor.Specialty, doctor.Bio, doctor.ConsultationFee));
+
+        if (doctor == null)
+            return Result<DoctorDetailsDto>.Failure("Doctor not Found!");
+
+        return Result<DoctorDetailsDto>.Success(new DoctorDetailsDto
+        {
+            Id = doctor.Id,
+            Specialty = doctor.Specialty,
+            Bio = doctor.Bio,
+            Fee = doctor.ConsultationFee
+        });
     }
 }

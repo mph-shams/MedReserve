c97c7fe044ccf3640970087e6ddae437d57bc80f
Parsanb <parsanasiri123@gmail.com>
2026-01-06 16:27:05 +0330

    Create DTOs : SystemReportDto

---
+++ Application/DTOs/Admin/SystemReportDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MedReserve.Application.DTOs.Admin
+{
+    public class SystemReportDto
+    {
+        public int TotalAppointments { get; set; }
+        public int DoneAppointments { get; set; }
+        public int TotalDoctors { get; set; }
+        public decimal TotalRevenue { get; set; }
+    }
+
+}

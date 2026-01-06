cfb4a299eb2cb0663fe8d7462d96e62c51eaf56f
milad <miladhamedani81@gmail.com>
2026-01-06 22:33:49 +0330

    Update DTOs : AuthModels

--- Application/DTOs/Auth/AuthModels.cs
+++ Application/DTOs/Auth/AuthModels.cs
@@ -1,5 +1,5 @@
 ﻿namespace Application.DTOs.Auth;
 
-public record RegisterRequest(string Username, string Password, int RoleId);
+public record RegisterRequest(string Username, string Password, int RoleId, long? TelegramChatId = null);
 public record LoginRequest(string Username, string Password);
 public record AuthResponse(string AccessToken, Guid RefreshToken, string Username, string Role);

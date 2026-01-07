445157f205c6f854f52fd70bd36086fe9e54a075
Parsanb <parsanasiri123@gmail.com>
2026-01-07 22:38:08 +0330

    Create DTOs : CreateScheduleRequest

---
+++ Application/DTOs/Schedules/CreateScheduleRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace MedReserve.Application.DTOs.Schedules
+{
+    public class CreateScheduleRequest
+    {
+
+        public int DayOfWeek { get; set; }
+        public string StartTime { get; set; } = string.Empty;
+        public string EndTime { get; set; } = string.Empty;
+    
+    }
+
+}

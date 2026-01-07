10e554a8a8fdea3fa492c6a93d155bf1956c0c51
Parsanb <parsanasiri123@gmail.com>
2026-01-07 22:50:55 +0330

    Create Queries : GetDoctorSchedulesQuery

---
+++ Application/Features/Schedules/Queries/GetDoctorSchedules/GetDoctorSchedulesQuery.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using Domain.Entities;
+using MedReserve.Application.DTOs.Doctors;
+using MediatR;
+
+namespace Application.Features.Schedules.Queries.GetDoctorSchedules;
+
+public class GetDoctorSchedulesQuery : IRequest<Result<List<ScheduleDto>>>
+{
+    public int DoctorId { get; set; }
+}
+
+public class GetDoctorSchedulesHandler(IUnitOfWork _unitOfWork) : IRequestHandler<GetDoctorSchedulesQuery, Result<List<ScheduleDto>>>
+{
+    public async Task<Result<List<ScheduleDto>>> Handle(GetDoctorSchedulesQuery request, CancellationToken ct)
+    {
+        var schedules = (await _unitOfWork.Repository<Schedule>().GetAllAsync())
+            .Where(s => s.DoctorId == request.DoctorId)
+            .OrderBy(s => s.DayOfWeek)
+            .Select(s => new ScheduleDto
+            {
+                Id = s.Id,
+                DayOfWeek = s.DayOfWeek,
+                StartTime = s.StartTime,
+                EndTime = s.EndTime,
+                SlotDuration = s.SlotDuration
+            })
+            .ToList();
+
+        return Result<List<ScheduleDto>>.Success(schedules);
+    }
+}

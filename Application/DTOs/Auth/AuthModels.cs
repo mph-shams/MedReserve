1b13d6b69b69a98624cd82a8af7e57bebc30efb9
hossein2081 <117431730+hossein2081@users.noreply.github.com>
2026-01-08 12:51:36 +0330

    Update Solution : MedReserve.Application

--- Application/DTOs/Auth/AuthModels.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Application.DTOs.Auth;
-
-public record RegisterRequest(string Username, string Password, int RoleId, long? TelegramChatId = null);
-public record LoginRequest(string Username, string Password);
-public record AuthResponse(string AccessToken, Guid RefreshToken, string Username, string Role);
--- Application/Features/Appointments/Commands/CreateAppointment/CreateAppointmentCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using Application.Common.Models;
-
-namespace MedReserve.Application.Features.Appointments.Commands.CreateAppointment;
-
-public record CreateAppointmentCommand(int DoctorId, int PatientId, DateTime AppointmentDate, string Description) : IRequest<Result<int>>;
